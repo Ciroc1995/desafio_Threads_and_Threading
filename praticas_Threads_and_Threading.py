5bfa6ee3646de3d63ef4557f53594e48046a5902
Cicero Cordeiro <cicero.cordeiro1995@gmail.com>
2023-01-31 10:43:21 -0300

    Update praticas_Threads_and_Threading.py

--- praticas_Threads_and_Threading.py
+++ praticas_Threads_and_Threading.py
@@ -15,3 +15,25 @@ nova_thread.start()
 navegar_ate_o_site('https://www.globoesporte.com')
 nova_thread.join()
 
+# Desafio 2
+
+def navegar_ate_o_site(site, produto):
+    print(f'Navegando ate o site {site} e extraindo valores do produto {produto}')
+
+
+produtos = ['celular', 'monitor', 'fone-de-ouvido', 'alto-falante', 'computador']
+threads = []
+for produto in produtos:
+    nova_thread = threading.Thread(
+        target=navegar_ate_o_site, args=('https://shopee.com.br', produto))
+    threads.append(nova_thread)
+
+for thread in threads:
+    thread.start()
+    print(f'Inicializando thread {thread.name}')
+
+for thread in threads:
+    thread.join()
+    print(f'Finalizando thread {thread.name}')
+
+
